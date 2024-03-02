a7edc4cb50e06adc28ecb8dd6560efa4d02d726e
Anthony Tsui <68526943+s3791898@users.noreply.github.com>
2024-03-02 13:10:16 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Project Template Repository
+
+This repository serves as a template for setting up new projects. It contains common files and configurations that can be used across various projects, saving time and ensuring consistency.
+
+## License
+This project is licensed under the MIT License.
---
+++ webpack.common.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      favicon: "./src/assets/icons/favicon.ico",
+      filename: "index.html",
+      inject: "head",
+      scriptLoading: "defer",
+    }),
+  ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
